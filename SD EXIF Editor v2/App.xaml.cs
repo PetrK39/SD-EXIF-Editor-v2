334b5c0098c0da7f6ecf4f652caa0160ee2adde8
PetrK39 <60184450+PetrK39@users.noreply.github.com>
2024-08-04 17:14:29 +0300

    Borrowed code from v1 project

--- SD EXIF Editor v2/App.xaml.cs
+++ SD EXIF Editor v2/App.xaml.cs
@@ -1,5 +1,10 @@
-﻿using System.Configuration;
+﻿using SD_EXIF_Editor_v2.Model;
+using SD_EXIF_Editor_v2.View;
+using SD_EXIF_Editor_v2.ViewModel;
+using System.Configuration;
 using System.Data;
+using System.Diagnostics;
+using System.IO;
 using System.Windows;
 
 namespace SD_EXIF_Editor_v2
@@ -9,6 +14,28 @@ namespace SD_EXIF_Editor_v2
     /// </summary>
     public partial class App : Application
     {
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            if (e.Args.Length == 0 || e.Args.Length > 1) ExitWithMessage($"Usage: {Process.GetCurrentProcess().MainModule.ModuleName} %path_to_png_file");
+
+            var file = new FileInfo(e.Args[0]);
+
+            if (!file.Exists) ExitWithMessage($"File not exists \"{file.FullName}\"");
+            if (file.Extension != ".png") ExitWithMessage("Only .png images are supported");
+
+            var im = new Image(file.FullName);
+            var mvm = new MainViewModel(im);
+            var mv = new MainWindow(mvm);
+
+            mv.ShowDialog();
+
+            Shutdown();
+        }
+        private void ExitWithMessage(string message)
+        {
+            MessageBox.Show(message, "SD EXIF Editor", MessageBoxButton.OK);
+            Environment.Exit(0);
+        }
     }
 
 }
---
+++ SD EXIF Editor v2/View/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using SD_EXIF_Editor_v2.ViewModel;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace SD_EXIF_Editor_v2.View
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow(MainViewModel mvm)
+        {
+            InitializeComponent();
+            DataContext = mvm;
+
+            CommandBindings.Add(new CommandBinding(ApplicationCommands.Close, CloseCommandExecuted));
+            CommandBindings.Add(new CommandBinding(ApplicationCommands.Save, mvm.SaveCommandExecuted));
+            CommandBindings.Add(new CommandBinding(ApplicationCommands.Copy, mvm.CopyCommandExecuted));
+            CommandBindings.Add(new CommandBinding(ApplicationCommands.Delete, mvm.DeleteCommandExecuted));
+
+            TextBox.TextChanged += TextBox_TextChanged;
+            TextBox.Focus();
+        }
+
+        private void TextBox_TextChanged(object sender, RoutedEventArgs e)
+        {
+            TextBox.TextChanged -= TextBox_TextChanged;
+            TextBox.CaretIndex = TextBox.Text.Length;
+        }
+
+        private void CloseCommandExecuted(object sender, ExecutedRoutedEventArgs e)
+        {
+            Close();
+        }
+    }
+}
