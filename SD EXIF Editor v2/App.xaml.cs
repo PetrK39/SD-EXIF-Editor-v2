68c7871e7d0936e1bb5b189fdf05047cecaed699
PetrK39 <60184450+PetrK39@users.noreply.github.com>
2024-08-04 16:05:54 +0300

    Добавьте файлы проекта.

---
+++ SD EXIF Editor v2/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace SD_EXIF_Editor_v2
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
